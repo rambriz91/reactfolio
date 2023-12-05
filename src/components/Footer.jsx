8416b82b8411f135e25a8f88241972911de9aeda
rambriz91 <rambriz91@gmail.com>
2023-12-05 10:28:53 -0800

    reformatted the footer component to include logo links.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,35 @@
 function Footer() {
   return (
     <footer>
-      <p>🦊🦊🦊🦊 Copyright 2023 🦊🦊🦊🦊</p>
+      <ul className='contact-list'>
+        <li>
+          <a href='https://github.com/rambriz91'>
+            <img
+              src='/images/github-mark-white.png'
+              alt='Octocat logo'
+              className='logo'
+            />
+          </a>
+        </li>
+        <li>
+          <a href='https://www.instagram.com/sans_armour/'>
+            <img
+              src='/images/Instagram_Glyph_White.png'
+              alt='instagram logo'
+              className='logo'
+            />
+          </a>
+        </li>
+        <li>
+          <a href='https://www.linkedin.com/in/robert-ambriz-59281493/'>
+            <img
+              src='/images/icons8-linkedin-500.png'
+              alt='LinkedIn Logo'
+              className='logo'
+            />
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,6 +11,10 @@ export default function Contact() {
                 <input type='text' />
               </div>
               <div>
+                <h3>Email</h3>
+                <input type='text' />
+              </div>
+              <div>
                 <h3>Message</h3>
                 <textarea type='text' />
               </div>
@@ -20,36 +24,6 @@ export default function Contact() {
             </div>
           </div>
         </form>
-
-        <ul className='contact-list'>
-          <li>
-            <a href='https://github.com/rambriz91'>
-              <img
-                src='/images/github-mark-white.png'
-                alt='Octocat logo'
-                className='logo'
-              />
-            </a>
-          </li>
-          <li>
-            <a href='https://www.instagram.com/sans_armour/'>
-              <img
-                src='/images/Instagram_Glyph_White.png'
-                alt='instagram logo'
-                className='logo'
-              />
-            </a>
-          </li>
-          <li>
-            <a href='https://www.linkedin.com/in/robert-ambriz-59281493/'>
-              <img
-                src='/images/icons8-linkedin-500.png'
-                alt='LinkedIn Logo'
-                className='logo'
-              />
-            </a>
-          </li>
-        </ul>
       </section>
     </>
   );
