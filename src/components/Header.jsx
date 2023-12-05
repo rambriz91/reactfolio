183c0284e28755d19ad87ba5aa4adcbd46cebe7b
rambriz91 <rambriz91@gmail.com>
2023-12-04 17:53:57 -0800

    moved png to public.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,7 @@ function Header() {
   //renders Navigation tabs within the header component.
   return (
     <header>
-      <img
-        src='src/assets/images/name-text.png'
-        alt='Stylized Text Rob Ambriz'
-      />
+      <img src='/images/name-text.png' alt='Stylized Text Rob Ambriz' />
       <Navigation />
     </header>
   );
